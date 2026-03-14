315937746c9f401bc9a592e209ebab77a892b890
Anna Orlova <orlovanna412@gmail.com>
2026-03-14 00:56:45 +0000

    add book object constructor + storage

---
+++ library.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
+};
+
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
