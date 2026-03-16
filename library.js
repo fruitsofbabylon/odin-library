a29a8d5ab520f425d840ca48e6221c10bdcee560
Anna Orlova <orlovanna412@gmail.com>
2026-03-16 19:56:10 +0000

    display all books in array as cards

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookCards = document.querySelector(".book-cards");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -12,4 +13,48 @@ function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
+};
+
+function addBookCard() {
+  for (const book of myLibrary) {
+    const card = document.createElement('div');
+    card.className = 'card';
+    
+    const title = document.createElement('h2');
+    title.textContent = book.title;
+    
+    const author = document.createElement('p');
+    author.textContent = book.author;
+    
+    const pages = document.createElement('p');
+    pages.textContent = `${book.pages} pages`;
+    
+    const checkboxDiv = document.createElement('div');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = `${book.id}`;
+    checkbox.name = 'readbook';
+    checkbox.value = book.read ? 'Read' : 'Not Read';
+    checkbox.checked = book.read;
+    
+    const label = document.createElement('label');
+    label.htmlFor = `${book.id}`;
+    label.textContent = book.read ? 'Read' : 'Not read yet';
+    
+    checkboxDiv.appendChild(checkbox);
+    checkboxDiv.appendChild(label);
+    
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(checkboxDiv);
+    card.appendChild(removeBtn);
+    
+    bookCards.appendChild(card);
+  };
 }
+
+addBookCard();
